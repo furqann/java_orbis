c64342c1b8c521faa9db6fc1c44f0ccaf9d465df
Furqan Ahmad <furqanahmad21@hotmail.com>
2019-05-25 23:42:43 +0500

    Simple print of list using lambda and reference method

--- src/main/java/com/orbis/java/IntegerStreams.java
+++ src/main/java/com/orbis/java/IntegerStreams.java
@@ -1,4 +1,15 @@
 package com.orbis.java;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class IntegerStreams {
+    private IntegerStreams(){}
+
+    private static List<Integer> numbers = asList(1,2,34,5,6,7,8,9);
+
+    public static void showNumbers(){
+        numbers.forEach(System.out::println);
+    }
 }
