808c562a6a9f217da50c67f716a6ab103b76eb00
Furqan Ahmad <furqanahmad21@hotmail.com>
2019-07-07 18:06:28 +0500

    If file does not exist then create one

--- src/main/java/com/orbis/java/handling/file/WritingToFile.java
+++ src/main/java/com/orbis/java/handling/file/WritingToFile.java
@@ -8,6 +8,9 @@ public class WritingToFile {
     public static void main(String[] args) throws IOException {
         //Simple Writing
         File file = new File("simple_write_file.txt");
+        if(!file.exists()){
+            file.createNewFile();
+        }
         FileWriter writer = new FileWriter(file,true);
         writer.write("This is the first line\n");
         writer.write("This is the Second line\n");
@@ -16,6 +19,9 @@ public class WritingToFile {
 
         //Append at the end of file
         File appendFile = new File("append_write_file.txt");
+        if(!appendFile.exists()){
+            file.createNewFile();
+        }
         FileWriter append_writer = new FileWriter(appendFile,true);
         append_writer.write("This is the first line\n");
         append_writer.write("This is the Second line\n");
